19b59b4bc4368726f9904eff03d477ac06e1e033
yannick.heinrich <yannick.heinrich@f18eedd0-de4d-11de-b4bf-3fb2a63344f9>
2009-12-04 15:01:05 +0000

    Fluke the shake up

--- src/jeudecarac/MainFrame.java
+++ src/jeudecarac/MainFrame.java
@@ -2,15 +2,21 @@
 package jeudecarac;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class MainFrame extends JFrame {
 
     EvtCarac evtCarac;
+    
+
 
     /** constructeur */
     public MainFrame() {
         evtCarac = EvtCarac.getInstance();
+        evtCarac.generateCarac();
 
         //Ajoute un écouteur pour les touches appuyées
         addKeyListener(new java.awt.event.KeyAdapter() {
@@ -24,10 +30,12 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().add(new PanelCarac()); //ajoute le Panel à la fenetre
         setSize(new Dimension(300, 400));
+
     }
 
     /** Fction appelée quand une touche est préssée */
     private void formKeyPressed(java.awt.event.KeyEvent evt) {
         evtCarac.setCarac(evt.getKeyChar());
+        //evtCarac.generateCarac();
     }
 }
--- src/jeudecarac/PanelCarac.java
+++ src/jeudecarac/PanelCarac.java
@@ -3,29 +3,52 @@ package jeudecarac;
 
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class PanelCarac extends JPanel implements Observer {
 
     EvtCarac evtCarac;
+    int positionCaract;
+    int time;
+    Timer tim;
+
 
     /** constructeur */
     public PanelCarac() {
         evtCarac = EvtCarac.getInstance();
         evtCarac.addObserver(this); //on observe evtCarac.
+        positionCaract = 0;
+        time=(int)(Math.random()*30+10);
+        ActionListener taskPerformer = new ActionListener() {
+
+            public void actionPerformed(ActionEvent evt) {
+                positionCaract+=5;
+                repaint();
+            }
+        };
+        tim = new Timer(time, taskPerformer);
+        tim.start();
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);    //repeindre le font (classe parent)
         g.setFont(new Font("sansserif", Font.BOLD, 60));    //police
-        g.drawString("" + evtCarac.getCarac(), 100, 100);     //tracer le caractère
+        if(positionCaract >= this.getWidth()){
+            positionCaract=0;
+            evtCarac.generateCarac();
+        }
+        g.drawString("" + evtCarac.getCarac(), positionCaract, 100);     //tracer le caractère
     }
 
     /** Fonction appelée automatiquement lorsqu'un élément observé est modifié (ici evtCarac) */
     public void update(Observable o, Object arg) {
+        tim.setDelay(evtCarac.getTime());
         repaint();
     }
 }
