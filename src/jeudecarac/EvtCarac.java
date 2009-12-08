2a87d9f6bd1a65cd74816ed6e88e74ae2d283832
yannick.heinrich <yannick.heinrich@f18eedd0-de4d-11de-b4bf-3fb2a63344f9>
2009-12-08 08:27:06 +0000

    Ca gère

--- src/jeudecarac/EvtCarac.java
+++ src/jeudecarac/EvtCarac.java
@@ -6,7 +6,7 @@ import java.util.Observable;
 public class EvtCarac extends Observable {   //classe observable
 
     private static EvtCarac evtCarac;
-    private char carac;                 //le caractère à affiche
+    private char typedCarac;                 //le caractère à affiche
     private char randomCarac;
     private int time;
 
@@ -22,7 +22,7 @@ public class EvtCarac extends Observable {   //classe observable
     }
 
     private EvtCarac() {
-        carac = ' ';
+        typedCarac = ' ';
        randomCarac = ' ';
     }
 
@@ -38,14 +38,18 @@ public class EvtCarac extends Observable {   //classe observable
     public void setCarac(char carac) {
         //on vérifie que c'est une lettre. Sinon on ne prévient personne.
         if (carac >= 97 && carac <= 122) {
-            this.carac = carac;
+            this.typedCarac = carac;
             
             this.setChanged();      //notre état a changé
             this.notifyObservers(); //on prévient tout ceux qui observent.
         }
     }
 
-    public char getCarac() {
+    public char getRandomCarac() {
         return randomCarac;
+    }
+
+    public char getTypedCarac(){
+        return typedCarac;
     }
 }
--- src/jeudecarac/MainFrame.java
+++ src/jeudecarac/MainFrame.java
@@ -1,22 +1,55 @@
 //Fenetre principale
 package jeudecarac;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.Timer;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
 
     EvtCarac evtCarac;
-    
-
+    private PanelCarac panelCarac;
+    private JButton startButton;
+    private JButton stopButton;
+    private JButton quitButton;
+    private JLabel scoreLabel;
+    private JLabel typedLetter;
 
     /** constructeur */
     public MainFrame() {
+        super();
         evtCarac = EvtCarac.getInstance();
         evtCarac.generateCarac();
+
+        //Init panel
+        panelCarac = new PanelCarac();
+        //init Button
+        startButton = new JButton("Start");
+        stopButton = new JButton("Stop");
+        quitButton = new JButton("Quit");
+        scoreLabel = new JLabel("Score = 0");
+        typedLetter = new JLabel(" ");
+        typedLetter.setHorizontalAlignment(JLabel.CENTER);
+        typedLetter.setFont(new Font("sansserif", Font.BOLD,30 ));
+
+        JPanel pan = new JPanel();
+        pan.setLayout(new FlowLayout());
+        pan.add(startButton);
+        pan.add(stopButton);
+        pan.add(quitButton);
+        pan.add(scoreLabel);
+
+
+        startButton.setEnabled(false);
+        startButton.setFocusable(false);    //pour que la fenetre attrape l'évenement "touche appuyée"
+        quitButton.setFocusable(false);
+        stopButton.setFocusable(false);
 
         //Ajoute un écouteur pour les touches appuyées
         addKeyListener(new java.awt.event.KeyAdapter() {
@@ -27,15 +60,58 @@ public class MainFrame extends JFrame {
             }
         });
 
+        quitButton.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                quit();
+            }
+        });
+
+        stopButton.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                panelCarac.stopTimer();
+                startButton.setEnabled(true);
+                stopButton.setEnabled(false);
+
+            }
+        });
+
+        startButton.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                panelCarac.startTimer();
+                startButton.setEnabled(false);
+                stopButton.setEnabled(true);
+            }
+        });
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().add(new PanelCarac()); //ajoute le Panel à la fenetre
+
+        //on ajoute les éléments graphiques
+        this.setLayout(new BorderLayout());
+        getContentPane().add(typedLetter, BorderLayout.NORTH);
+        getContentPane().add(panelCarac, BorderLayout.CENTER); //ajoute le Panel à la fenetre
+        getContentPane().add(pan, BorderLayout.SOUTH);
+
+        //Ajout dasn kle flux
+
         setSize(new Dimension(300, 400));
+
+
+
 
     }
 
     /** Fction appelée quand une touche est préssée */
     private void formKeyPressed(java.awt.event.KeyEvent evt) {
         evtCarac.setCarac(evt.getKeyChar());
-        //evtCarac.generateCarac();
+        typedLetter.setText("You typed: " + evtCarac.getTypedCarac());     //affichage du caractère tapé
+    }
+
+    public void quit() {
+
+        this.dispose();
+        System.exit(0);
     }
 }
